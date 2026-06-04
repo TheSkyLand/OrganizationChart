4ccedfc458e64db7582a9a15648dc15a279001aa
TheSkyLand <Aradioactiveplay@gmail.com>
2026-06-04 18:41:37 +0300

    did nothing i swear

--- src/pages/Chart.tsx
+++ src/pages/Chart.tsx
@@ -2,13 +2,87 @@
 
 
 const Chart = () => {
+    const countries = ["Netherlands nl", "USA us"]
+    const jaggedArr = {
+        users: ["john11", "alex24", "brandon65"],
+        admins: ["peter25"],
+        owners: ["george99"]
+    }
+
+    const block = document.querySelector('') 
+    const rect = block.getBoundingClientRect();
 
     
-    return(
-        <div>
-            hi im am a chart!
+    return (
 
+        <div
+
+        >
+            hi i am a chart!
+
+
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    width: 1000,
+                    height: 1000
+                }}
+            >
+
+                {/* Chart block */}
+                <div
+                    style={{
+                        width: 100,
+                        height: 100,
+                        border: "1px solid black",
+                    }}
+                >
+                    dfhhdfhfdh';fhd';
+                </div>
+
+                {/* Chart connection */}
+                <svg width="250" height="250">
+                    <line x1="100" y1="10050" x2="100" y2="100" stroke="black" stroke-width="2" />
+                </svg>
+
+
+                {/* Child blocks */}
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "row"
+                    }}
+                >
+                    {jaggedArr.users.map((item, key) => (
+                        <div
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center"
+
+                            }}>
+                            <div>
+                                .
+                            </div>
+                            <div
+                                style={{
+                                    border: "1px solid black",
+                                    padding: 25
+                                }}
+                            >
+                                {item}
+                            </div>
+                        </div>
+                    ))}
+
+
+                </div>
+            </div>
         </div>
+
     )
 }
 
